342c95f844a17b15e3ad33d96f1948e15372a65f
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-31 14:26:56 +0100

    add comment

--- src/enigma/AppProvider.js
+++ src/enigma/AppProvider.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, createContext } from "react";
 import { openSession, closeSession } from "./configSession";
 
+//
 export const AppContext = createContext();
 
+// A wrapper to maintain session open and close
 const AppProvider = ({ children }) => {
    const [app, setApp] = useState();
 
--- src/enigma/configSession.js
+++ src/enigma/configSession.js
@@ -2,6 +2,7 @@ import enigma from "enigma.js";
 import schema from "enigma.js/schemas/12.67.2.json";
 import SenseUtilities from "enigma.js/sense-utilities";
 
+// enviroment variables - qlik server
 const configs = {
    host: process.env.REACT_APP_QLIK_HOST,
    secure: process.env.REACT_APP_QLIK_SECURE,
@@ -10,16 +11,16 @@ const configs = {
    appId: process.env.REACT_APP_QLIK_APPID
 };
 
+// create session
 const url = SenseUtilities.buildUrl(configs);
-
 const session = enigma.create({ schema, url });
 
+// open and close methods
 const openSession = async () => {
    const qix = await session.open();
-   const document = await qix.openDoc("Insurance Claims 2020.qvf");
+   const document = await qix.openDoc(configs.appId);
    return document;
 };
-
 const closeSession = async () => await session.close();
 
 export { openSession, closeSession };
